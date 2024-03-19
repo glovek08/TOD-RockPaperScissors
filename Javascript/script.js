568be890e17d2a22a059d61af3d3fe17fbd32841
Gabriel Barn <gbrielbbarn@gmail.com>
2024-03-19 19:36:57 -0300

    [feat] getUserPick function

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,3 +3,29 @@
 /* Create a function that takes both variables and compares them with the following logic: 
     Rock > Scissors, Scissors > Paper, Paper > Rock */
 /* Reset the variables and radio inputs, and return the winning player */
+/*From here down, only the solution specified in the project.*/
+document.addEventListener('DOMContentLoaded', () => {
+    const radioButtons = document.getElementsByName("user-pick");
+    const submitButton = document.getElementById('submit');
+    submitButton.addEventListener('click', () => {
+        console.log(getUserPick());
+    })
+    const getUserPick = () => {
+        let userPick = '';
+        for (let i = 0; i < radioButtons.length; i++) {
+            if (radioButtons[i].checked) {
+                userPick = radioButtons[i].value;
+                break;
+            }
+        }
+        return userPick;
+    }
+})
+const getComputerChoice = () => {
+    let randomNumber = Math.floor(Math.random() * 2) /* 0 = Rock, 1 = Paper, 2 = Scissor */;
+    if (randomNumber === 0) {
+        return 'Rock';
+    } else if (randomNumber === 1) {
+        return 'Paper';
+    }
+}
