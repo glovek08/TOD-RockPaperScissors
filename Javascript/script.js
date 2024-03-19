90363847486d7d873c345b6109b8fa14dbaf3a08
Gabriel Barn <131347941+glovek08@users.noreply.github.com>
2024-03-19 16:41:42 -0300

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,5 @@
+/* Create a variable to store the user pick from amongst three options */
+/* Create a variable to store a randomly generated number within 1-3 that corresponds to the computer's pick */
+/* Create a function that takes both variables and compares them with the following logic: 
+    Rock > Scissors, Scissors > Paper, Paper > Rock */
+/* Reset the variables and radio inputs, and return the winning player */
